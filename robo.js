206e89c1f8536642baf306a069440fadc231eaa2
Victor <victorari856@gmail.com>
2021-11-02 14:19:40 -0300

    Update

---
+++ robo.js
@@ -0,0 +1,24 @@
+const utterance = new SpeechSynthesisUtterance();
+
+utterance.lang = "pt-br";
+utterance.rate = 1;
+
+function speak() {
+    speechSynthesis.speak(utterance);
+}
+function stop() {
+    speechSynthesis.cancel();
+}
+
+function reset() {
+    speechSynthesis.cancel();
+    speechSynthesis.speak(utterance);
+}
+
+var onclick="speak()">Escutar
+var onclick="stop()">Parar
+var onclick="reset()"> Reniciar
+
+function setText(event) {
+    utterance.text = event.target.innerText;
+}
